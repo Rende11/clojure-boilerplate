22804dc9e75b056e4ec1008b7ff425eddf79db3f
rende11 <artamonovn@gmail.com>
2019-01-06 01:19:29 +0400

    fail build

--- linter.sh
+++ linter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [$(find ./src -type f -exec joker --lint {} \; 2>&1 | wc -l) -ne 0 ]; then
-    find ./src -type f -exec joker --lint {} \;
+if [$(find ./test -type f -exec joker --lint {} \; 2>&1 | wc -l) -ne 0 ]; then
+    find ./test -type f -exec joker --lint {} \;
     exit 1
 else
     echo "no errors found"

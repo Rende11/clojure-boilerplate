c3127b5f52fd7b3539008cf2bde3a3cc3abf7c49
rende11 <artamonovn@gmail.com>
2019-01-05 21:13:15 +0400

    another try

--- linter.sh
+++ linter.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-find ./src -type f -exec joker --lint {} \; &> log
-
-if [$(cat log | wc -l) -ne 0 ]; then
-    cat log
+if [$(find ./src -type f -exec joker --lint {} \; 2>&1 | wc -l) -ne 0 ]; then
+    find ./src -type f -exec joker --lint {} \;
     exit 1
 else
     echo "no errors found"

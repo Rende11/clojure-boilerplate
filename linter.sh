8a08d279b75a92a39a925bf3c6df4a8e1c856232
rende11 <artamonovn@gmail.com>
2019-01-06 23:30:13 +0400

    add spaces

--- linter.sh
+++ linter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [$(find ./test -type f -exec joker --lint {} \; 2>&1 | wc -l) -ne 0 ]; then
+if [ $(find ./test -type f -exec joker --lint {} \; 2>&1 | wc -l) -ne 0 ]; then
     find ./test -type f -exec joker --lint {} \;
     exit 1
 else

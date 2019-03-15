58c7a21bdae4a06910fabce4589133cf9a99c825
rende11 <artamonovn@gmail.com>
2019-03-15 21:55:00 +0400

    ...

---
+++ src/client/main/dev.cljs
@@ -0,0 +1,16 @@
+(ns ^:figwheel-no-load client.main.dev
+  (:require [client.main.core :as core]
+            [figwheel.client :as figwheel :include-macros true]
+            [re-frisk.core :refer [enable-re-frisk!]]
+            [devtools.core :as devtools]))
+
+(devtools/install!)
+
+(enable-console-print!)
+
+(figwheel/watch-and-reload
+ :websocket-url "ws://localhost:3449/figwheel-ws"
+ :jsload-callback core/main)
+
+(enable-re-frisk!)
+(core/main)
